39e5404f08e67b5aa6d126d787d7a611f3c4b30d
Tsjippy <ewaldha@hotmail.com>
2025-09-26 11:42:04 +0100

    changed: classnames replace _ with -

--- php/recaptcha_html.php
+++ php/recaptcha_html.php
@@ -16,12 +16,12 @@ function getRecaptchaHtml(){
             ?>
             <input type='hidden' name='g-recaptcha-response' id='g-recaptcha-response'>
             <script>
-                document.querySelectorAll('.submit_wrapper .form_submit').forEach(el=>el.disabled=true);
+                document.querySelectorAll('.submit-wrapper .form-submit').forEach(el=>el.disabled=true);
                 function onloadCallback(){
                     grecaptcha.ready(function() {
                         setInterval(function(){
                             grecaptcha.execute('<?php echo $recaptcha['key'];?>', {action: 'validate_captcha'}).then(function(token) {
-                                document.querySelectorAll('.submit_wrapper .form_submit[disabled]').forEach(el=>el.disabled=false);
+                                document.querySelectorAll('.submit-wrapper .form-submit[disabled]').forEach(el=>el.disabled=false);
                                 console.log( 'refreshed token:', token );
                                 document.getElementById('g-recaptcha-response').value = token;
                             });
